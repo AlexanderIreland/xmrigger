d3727887627e1671b7b2ad87e07b372ed66cb304
AlexanderIreland <36182023+AlexanderIreland@users.noreply.github.com>
2020-10-19 21:08:22 -0500

    Update pre-rig.sh

--- pre-rig.sh
+++ pre-rig.sh
@@ -26,7 +26,15 @@ echo "# - - - - - - - - - - - - - - - - - - - - - - - -"
 #  6.b) Platform support
 #   6.b.1) L/Unix: -DCMAKE_SYSTEM_NAME=Linux
 #   6.b.2) MacOS: -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl
-#   6.b.3) Windows: -G "Unix Makefiles" -DXMRIG_DEPS=c:/xmrig-deps/gcc/x64
+#    6.b.2.a: 32-bit binary out: -DCMAKE_OSX_ARCHITECTURES=i386
+#    6.b.2.b: 64-bit binary out: -DCMAKE_OSX_ARCHITECTURES=x86_64
+#    6.b.2.c: 96-bit universal binary out: "-DCMAKE_OSX_ARCHITECTURES=x86_64;i386"
+#   6.b.3) Windows:
+#    6.b.3.a) MSYS2: "c:\Program Files\CMake\bin\cmake.exe" .. -G "Unix Makefiles" -DXMRIG_DEPS=c:/xmrig-deps/gcc/x64
+#    6.b.3.b) VS19: cmake .. -G "Visual Studio 16 2019" -A x64 -DXMRIG_DEPS=c:\xmrig-deps\msvc2019\x64
+#     cmake --build . --config Release
+#    6.b.3.c) VS19 + CUDA: cmake .. -G "Visual Studio 16 2019" -A x64 -DCUDA_TOOLKIT_ROOT_DIR="c:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.0"
+#     cmake --build . --config Release
 #   6.b.4) WindowsPhone: -DCMAKE_SYSTEM_NAME=WindowsPhone -DCMAKE_SYSTEM_VERSION=8.1
 #   6.b.5) Tegra/Android: -DCMAKE_SYSTEM_NAME=Android
 # 7) Configure make opts 
