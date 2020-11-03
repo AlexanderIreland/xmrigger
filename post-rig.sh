f0e95fe54d3a08477f0136699ac292a33bc43d09
AlexanderIreland <36182023+AlexanderIreland@users.noreply.github.com>
2020-11-02 18:31:13 -0600

    stuck-in-elevator
    Stuck in an elevator so I’m making a commit from my phone 🤷‍♂️
    
    Setting up a stub function for post-setup

--- post-rig.sh
+++ post-rig.sh
@@ -10,3 +10,12 @@ echo "# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 # 1) Throw fancy success message
 # 2) Verify wallet address
 # 3) Provide miner start option
+
+function start-miner () {
+  echo "I'm a stub function"
+  ./xmrig #should need no args, planning to gen config file 
+}
+
+function main () {
+  start-miner
+}
