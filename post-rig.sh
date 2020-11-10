251cf210243e371f7eec8672f15011de0e18c900
AlexanderIreland <36182023+AlexanderIreland@users.noreply.github.com>
2020-11-09 20:56:44 -0600

    overhaul
    Redid the logic, can launch with flags now

--- post-rig.sh
+++ post-rig.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
-echo "# ██████╗  ██████╗ ███████╗████████╗   ██████╗ ██╗ ██████╗ "
-echo "# ██╔══██╗██╔═══██╗██╔════╝╚══██╔══╝   ██╔══██╗██║██╔════╝ "
-echo "# ██████╔╝██║   ██║███████╗   ██║█████╗██████╔╝██║██║  ███╗"
-echo "# ██╔═══╝ ██║   ██║╚════██║   ██║╚════╝██╔══██╗██║██║   ██║"
-echo "# ██║     ╚██████╔╝███████║   ██║      ██║  ██║██║╚██████╔╝"
-echo "# ╚═╝      ╚═════╝ ╚══════╝   ╚═╝      ╚═╝  ╚═╝╚═╝ ╚═════╝ "
-echo "# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
 #todo
 # 1) Throw fancy success message
 # x) Verify wallet address
 # 3) Provide miner start option
 
-function start-miner-simple () {
+BASIC_START="false"
+SILENT_START="false"
+
+function intro-text () {
+  echo "${CYAN}+---------------------------------------------------------+"
+  echo "${PURPLE}# ██████╗  ██████╗ ███████╗████████╗   ██████╗ ██╗ ██████╗ "
+  echo "${PURPLE}# ██╔══██╗██╔═══██╗██╔════╝╚══██╔══╝   ██╔══██╗██║██╔════╝ "
+  echo "${PURPLE}# ██████╔╝██║   ██║███████╗   ██║█████╗██████╔╝██║██║  ███╗"
+  echo "${PURPLE}# ██╔═══╝ ██║   ██║╚════██║   ██║╚════╝██╔══██╗██║██║   ██║"
+  echo "${PURPLE}# ██║     ╚██████╔╝███████║   ██║      ██║  ██║██║╚██████╔╝"
+  echo "${PURPLE}# ╚═╝      ╚═════╝ ╚══════╝   ╚═╝      ╚═╝  ╚═╝╚═╝ ╚═════╝ "
+  echo "${CYAN}+---------------------------------------------------------+"
+}
+
+while getopts a:o:s:S:v: flag
+do
+    case "${flag}" in
+        bs) BASIC_START=true;;
+	      ss) SILENT_START=true;;
+    esac
+done
+
+
+function start-miner-basic () {
   echo "I'm a stub function"
   ./xmrig #should need no args, planning to gen config file 
 }
 
-function start-miner-screen () {
+function start-miner-silent () {
   # starts a screen that is immediately detatched - using /bin/bash instead of bash
   screen -dmS /bin/bash -c "./xmrig" xmrig
 }
 
 function main () {
-  start-miner-simple
+  intro-text
+  if [[ BASIC_START==true ]]
+  then
+	  start-miner-basic
+  else
+  start-miner-silent
 }
 
 
